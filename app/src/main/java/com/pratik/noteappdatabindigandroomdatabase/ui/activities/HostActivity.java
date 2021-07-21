f904e30a78192f0292b907e88c36272a21352cb2
Pratik Fagadiya <pratikfagadiya@gmail.com>
2021-07-21 23:54:09 +0530

    Add navigation component

---
+++ app/src/main/java/com/pratik/noteappdatabindigandroomdatabase/ui/activities/HostActivity.java
@@ -0,0 +1,16 @@
+package com.pratik.noteappdatabindigandroomdatabase.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pratik.noteappdatabindigandroomdatabase.R;
+
+public class HostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+    }
+}
